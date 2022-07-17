3ea3e417a4602f0c40b001ebc7130f2b4513fe32
makaserori3022 <2211140038j@ed.fuk.kindai.ac.jp>
2022-07-17 19:30:47 +0900

    falling foods

--- food.py
+++ food.py
@@ -3,6 +3,18 @@ from pygame.locals import *
 
 #画像の読み込み
 fried_chicken = pygame.image.load(os.path.join('images', 'FriedChicken.png'))
+chocolate = pygame.image.load(os.path.join('images', 'Chocolate.png'))
+iceCream = pygame.image.load(os.path.join('images', 'IceCream.png'))
+ramen = pygame.image.load(os.path.join('images', 'Ramen.png'))
+riceBall = pygame.image.load(os.path.join('images', 'RiceBall.png'))
+
+food_box = [fried_chicken,
+            chocolate,
+            iceCream,
+            ramen,
+            riceBall]
+
+
 
 class Food:
     def __init__(self, img, point, x, y):
@@ -15,6 +27,7 @@ class Food:
         screen.blit(self.food_img, (self.x, self.y))
     
     def fall(self):
-        self.y += 1
+        self.y += 10
+
             
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys, pygame
+import sys, pygame, random
 from pygame.locals import *
-import food
+from food import *
 
 pygame.init()
 size = width, height = (640, 720)
@@ -11,23 +11,41 @@ def main():
     pygame.display.set_caption("ONAKA GAME")
     x = width/2
     y = height-30
-    chicken = food.Food(food.fried_chicken, 0, 300, 0)
+    foods = []
+    clock = pygame.time.Clock()
+    FPS = 60
+    food_amount = 30
 
     while True:
+        clock.tick(FPS)
         screen.fill((0, 0, 0,))
         pygame.draw.circle(screen, (255,255,255),(x, y),30)
         pygame.draw.rect(screen, (255,255,255), (20,5,20,690), 10)
         
-        chicken.draw(screen)
-        chicken.fall()
+        if len(foods) <= 15:
+            for i in range(food_amount):
+                food = Food(random.choice(food_box), 0, random.randrange(45, width), random.randrange(-3000, -10))
+                foods.append(food)
+
+        for food in foods:
+            food.draw(screen)
+            food.fall()
+
+            if food.y > height + 50:#foodが画面外にあったら
+                foods.remove(food)
+               
+
+
+
+
         pygame.display.update()
 
 
         pressed_key = pygame.key.get_pressed()
         if pressed_key[K_LEFT] and  x > 30 +45 :
-            x-=1
+            x-=10
         if pressed_key[K_RIGHT] and x < width - 30:
-            x+=1
+            x+=10
         
     
 
